e3a543aea531bc93b5f4485419e9fc686829c3c2
Thomas Jemielita <thomasjemielita@gmail.com>
2019-06-18 16:21:26 -0400

    Add in "resample" plot option

--- R/plot.R
+++ R/plot.R
@@ -18,8 +18,15 @@ plot.PRISM = function(x, type="CI", ...){
   if (type=="CI"){
     # Combine parameter-estimates with rules ##
     parm = x$param.dat
-    rules = rbind(data.frame(Subgrps=0,Rules="Overall"), x$Rules)
-    plot.dat = left_join(parm, rules, by="Subgrps")
+    if (is.null(x$Rules)){
+      plot.dat = parm
+      plot.dat$Rules = ifelse(plot.dat$Subgrps==0, "Overall",
+                              as.character(plot.dat$Subgrps))
+    }
+    if (!is.null(x$Rules)){
+      rules = rbind(data.frame(Subgrps=0,Rules="Overall"), x$Rules)
+      plot.dat = left_join(parm, rules, by="Subgrps")
+    }
 
     # Order subgroups in descending order ##
     plot.dat$order = c(-1, rank(plot.dat$est[plot.dat$Subgrps>0]) )
@@ -36,6 +43,24 @@ plot.PRISM = function(x, type="CI", ...){
       theme_bw()
   }
   if (type=="resample"){
+    plot.dat = x$resamp.dist
+    if (is.null(x$Rules)){
+      plot.dat$Rules = ifelse(plot.dat$Subgrps==0, "Overall",
+                              as.character(plot.dat$Subgrps))
+    }
+    if (!is.null(x$Rules)){
+      rules = rbind(data.frame(Subgrps=0,Rules="Overall"), x$Rules)
+      plot.dat = left_join(plot.dat, rules, by="Subgrps")
+    }
+    res = ggplot(plot.dat, aes(est)) + geom_density() +
+      facet_wrap(~Rules) +
+      ggtitle("Bootstrap Distribution of Overall/Subgroup Estimates")+
+      theme(plot.title=element_text(size=16,face="bold"),
+            axis.text.y=element_blank(),
+            axis.ticks.y=element_blank(),
+            axis.text.x=element_text(face="bold"),
+            axis.title=element_text(size=12,face="bold"))+
+      theme_bw()
 
   }
   return(res)
