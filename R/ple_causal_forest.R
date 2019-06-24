ece59a212ea07426f7514ea6b47954f6caf027f1
Thomas Jemielita <thomasjemielita@gmail.com>
2019-06-24 17:57:45 -0400

    Added prediction function; added argument for estimating P(A|X) (treatment)

--- R/ple_causal_forest.R
+++ R/ple_causal_forest.R
@@ -11,15 +11,15 @@
 #' @param tune If TRUE, use grf automatic hyper-parameter tuning. If FALSE (default), no tuning.
 #' @param num.trees Number of trees (default=500)
 #' @param family Outcome type ("gaussian", "binomial"), default is "gaussian"
+#' @param mod.A Model for estimating P(A|X). Default is "mean" calculates the sample mean.
+#' If mod.A="RF", estimate P(A|X) using regression_forest (applicable for non-RCTs).
 #' @param ... Any additional parameters, not currently passed through.
 #'
 #' @import grf
 #'
-#' @return Patient-level estimates (E(Y|X,1), E(Y|X,0), E(Y|X,1)-E(Y|X,0)) for train/test sets.
+#' @return Trained causal_forest and regression_forest models.
 #'  \itemize{
 #'   \item mods - trained model(s)
-#'   \item mu_train - Patient-level estimates (training set)
-#'   \item mu_test - Patient-level estimates (test set)
 #' }
 #' @examples
 #' library(StratifiedMedicine)
@@ -29,17 +29,11 @@
 #' Y = dat_ctns$Y
 #' X = dat_ctns$X
 #' A = dat_ctns$A
-#' train = data.frame(Y, A, X)
-#' # Outcome/treatment must be labeled as Y/A #
 #'
 #'\donttest{
 #' # Default #
-#' mod1 = ple_causal_forest(train=train, Xtest=X)
-#' summary(mod1$mu_train$PLE)
-#'
-#' # Tune hyper-parameters (uncomment to run) #
-#' mod2 = ple_causal_forest(train=train, Xtest=X, tune=TRUE)
-#' summary(mod2$mu_train$PLE)
+#' mod1 = ple_causal_forest(Y, A, X, Xtest=X)
+#' summary(mod1$mu_train)
 #'
 #' }
 #'
@@ -48,38 +42,98 @@
 #' @seealso \code{\link{PRISM}}, \code{\link{causal_forest}}
 
 #### Causal_forest ###
-ple_causal_forest = function(Y, A, X, Xtest, tune=FALSE, num.trees=500, family="gaussian", ...){
+ple_causal_forest = function(Y, A, X, Xtest, tune=FALSE, num.trees=500, family="gaussian",
+                             mod.A = "mean", ...){
 
   set.seed(5131)
-  W = A
   ## Regression Forest: Y~X ##
   forest.Y = regression_forest(X, Y, ci.group.size=1, num.trees = min(500, num.trees) )
   Y.hat.train = predict(forest.Y)$predictions
   Y.hat.test = predict(forest.Y, newdata=Xtest)$predictions
   ## Regression Forest: W~X, If RCT ==> W is independent of X; use sample mean ##
-  W.hat.train = rep( mean(W), dim(X)[1] )
-  W.hat.test = rep( mean(W), dim(Xtest)[1] )
-  # forest.W = regression_forest(X, W, ci.group.size=1, num.trees = min(500, num.trees) )
-  # W.hat.train = predict(forest.W)$predictions
-  # W.hat.test = predict(forest.W, newdata=Xtest)$predictions
+  forest.A = mean(A)
+  A.hat.train = rep( mean(A), dim(X)[1] )
+  A.hat.test = rep( mean(A), dim(Xtest)[1] )
+  # forest.A = regression_forest(X, W, ci.group.size=1, num.trees = min(500, num.trees) )
+  # A.hat.train = predict(forest.A)$predictions
+  # A.hat.test = predict(forest.A, newdata=Xtest)$predictions
   ## Causal Forest ##
-  forest.CF = causal_forest(X, Y, W, tune.parameters = tune, num.trees=num.trees,
-                        W.hat=W.hat.train, Y.hat=Y.hat.train)
+  forest.CF = causal_forest(X, Y, W=A, tune.parameters = tune, num.trees=num.trees,
+                        W.hat=A.hat.train, Y.hat=Y.hat.train)
 
-  ## Predictions: Train/Test ##
-  PLE.train = forest.CF$predictions
-  mu0.train = Y.hat.train - W.hat.train * PLE.train
-  mu1.train = Y.hat.train + (1 - W.hat.train) * PLE.train
-
-  PLE.test = predict(forest.CF, newdata = Xtest)$predictions
-  mu0.test = Y.hat.test - W.hat.test * PLE.test
-  mu1.test = Y.hat.test + (1 - W.hat.test) * PLE.test
-
-  # Combine #
-  mu_train = data.frame(mu1 = mu1.train, mu0 = mu0.train,
-                        PLE = PLE.train)
-  mu_test = data.frame(mu1 = mu1.test, mu0 = mu0.test,
-                       PLE = PLE.test)
+  res = list(mods = list(forest.Y=forest.Y, forest.A=forest.A, forest.CF=forest.CF) )
+  class(res) = "ple_causal_forest"
   ## Return Results ##
-  return( list(mods = list(forest.Y, forest.CF), mu_train = mu_train, mu_test = mu_test) )
+  return( res )
 }
+
+#' Predict Patient-level Estimates: Causal Forest
+#'
+#' Get estimates of (E(Y|X,A=1), E(Y|X,A=0), E(Y|X,A=1)-E(Y|X,A=0)) using trained
+#' regression_forest and causal_forest model(s).
+#'
+#' @param object Trained random forest (ranger) model(s).
+#' @param newdata Data-set to make predictions at. For training data, should
+#' include covariates (X) and treatment (A) for oob predictions.
+#' @param ... Any additional parameters, not currently passed through.
+#'
+#' @import grf
+#'
+#' @return Data-frame with predictions of (E(Y|X,1), E(Y|X,0), E(Y|X,1)-E(Y|X,0))
+#'
+#' @examples
+#' library(StratifiedMedicine)
+#'
+#' ## Continuous ##
+#' dat_ctns = generate_subgrp_data(family="gaussian")
+#' Y = dat_ctns$Y
+#' X = dat_ctns$X
+#' A = dat_ctns$A
+#'
+#' \donttest{
+#' mod1 = ple_causal_forest(Y, A, X, Xtest=X)
+#' summary(mod1$mu_train)
+#' summary(predict(mod1, newdata=data.frame(A,X)))
+#' summary(predict(mod1, newdata=data.frame(X)))
+#' }
+#'
+#' @method predict ple_causal_forest
+#' @export
+#'
+#### Predict: ple_causal_forest ####
+predict.ple_causal_forest = function(object, newdata, ...){
+
+  forest.Y = object$mods$forest.Y
+  forest.A = object$mods$forest.A
+  forest.CF = object$mods$forest.CF
+  ## Use oob if possible (relevant for training data) ##
+  if ( "A" %in% names(newdata) ){
+    Y_hat = forest.Y$predictions
+    ## Regression Forest: W~X, If RCT ==> W is independent of X; use sample mean ##
+    if (is.numeric(forest.A)){
+      A_hat = rep( forest.A, dim(newdata)[1]  )
+    }
+    if (is.list(forest.A)){
+      A_hat = forest.A$predictions
+    }
+    PLE_hat = forest.CF$predictions
+  }
+  if (!("A" %in% names(newdata))){
+    # newdata = newdata[,!(colnames(newdata) %in% "A") ]
+    Y_hat = predict(forest.Y, newdata)$predictions
+    ## Regression Forest: W~X, If RCT ==> W is independent of X; use sample mean ##
+    if (is.numeric(forest.A)){
+      A_hat = rep( forest.A, dim(newdata)[1]  )
+    }
+    if (is.list(forest.A)){
+      A_hat = predict(forest.A, newdata)
+    }
+    PLE_hat = predict(forest.CF, newdata)$predictions
+  }
+  mu0_hat = Y_hat - A_hat * PLE_hat
+  mu1_hat = Y_hat + (1 - A_hat) * PLE_hat
+  mu_hat = data.frame(mu1 = mu1_hat, mu0 = mu0_hat, PLE = PLE_hat)
+  return( mu_hat  )
+}
+
+
