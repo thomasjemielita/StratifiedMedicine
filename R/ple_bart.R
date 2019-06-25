cc08cd0e435fa55ea5a61ee3e8797bcba5c5ea2a
Thomas Jemielita <thomasjemielita@gmail.com>
2019-06-25 16:34:57 -0400

    Include covariate by treatment interactions in model fit

--- R/ple_bart.R
+++ R/ple_bart.R
@@ -1,8 +1,8 @@
 #' Patient-level Estimates: BART
 #'
-#' Uses the BART algorithm (from BART R package) to obtain patient-level estimates.
-#' Used for continuous or binary outcomes, with output estimates of
-#' E(Y|X,A=a) and E(Y|X,A=1)-E(Y|X,A=0) (PLE). In progress.
+#' Uses the BART algorithm (Chipman et al 2010; BART R package) to obtain patient-level
+#' estimates. Used for continuous or binary outcomes. Covariate by treatment interactions
+#' are automatically included in BART model (as in Hahn et al 2017).
 #'
 #' @param Y The outcome variable. Must be numeric or survival (ex; Surv(time,cens) )
 #' @param A Treatment variable. (a=1,...A)
@@ -11,7 +11,6 @@
 #' @param family Outcome type ("gaussian", "binomial"), default is "gaussian"
 #' @param ... Any additional parameters, not currently passed through.
 #'
-#' @import BART
 #'
 #' @return Trained BART model(s) and patient-level estimates
 #' (E(Y|X,1), E(Y|X,0), E(Y|X,1)-E(Y|X,0)) for train/test sets.
@@ -33,30 +32,38 @@
 #'
 #' # BART #
 #' \donttest{
-#' mod1 = ple_bart(train=train, Xtest=X)
+#' require(BART)
+#' mod1 = ple_bart(Y, A, X, Xtest=X)
 #' summary(mod1$mu_train)
 #' summary(predict(mod1, newdata=X))
-#'
-#' summary(mod1$mu_train$PLE)
 #' }
 #'
-#' @seealso \code{\link{PRISM}}, \code{\link{BART}}
 #'
 #### BART ###
 ple_bart = function(Y, A, X, Xtest, family="gaussian", ...){
 
+  if (!requireNamespace("BART", quietly = TRUE)) {
+    stop("Package BART needed for ple_bart. Please install.")
+  }
+  ## Generate the covariate by treatment interactions ##
+  X_inter = X*A
+  colnames(X_inter) = paste(colnames(X), "_A", sep="")
+  W = cbind(X, A, X_inter)
   ## Generate counterfactual design matrices  ##
-  Xtrain_0 = data.frame(A=0, X)
-  Xtrain_1 = data.frame(A=1, X)
-  Xtest_0 = data.frame(A=0, Xtest)
-  Xtest_1 = data.frame(A=1, Xtest)
+  Xtrain_0 = data.frame(0, X, X*0)
+  colnames(Xtrain_0) = c( "A", colnames(X), paste(colnames(X), "_A", sep="") )
+  Xtrain_1 = data.frame(1, X, X*1)
+  colnames(Xtrain_1) = c( "A", colnames(X), paste(colnames(X), "_A", sep="") )
+  Xtest_0 = data.frame(0, Xtest, Xtest*0)
+  colnames(Xtest_0) = c( "A", colnames(Xtest), paste(colnames(Xtest), "_A", sep="") )
+  Xtest_1 = data.frame(1, Xtest, Xtest*1)
+  colnames(Xtest_1) = c( "A", colnames(Xtest), paste(colnames(Xtest), "_A", sep="") )
 
-  x.train = data.frame(A,X)
   # Test: Include both train/test for faster predictions #
   X.FULL = rbind(Xtrain_0, Xtrain_1, Xtest_0, Xtest_1)
   ## BART ##
   set.seed(51351)
-  bartFit = wbart(x.train = x.train, y.train = Y, x.test = X.FULL)
+  bartFit = BART::wbart(x.train = W, y.train = Y, x.test = X.FULL)
   n.tr = dim(Xtrain_0)[1]
   n.ts = dim(Xtest_0)[1]
   ### PLE Predictions: Train/Test ###
@@ -83,7 +90,6 @@ ple_bart = function(Y, A, X, Xtest, family="gaussian", ...){
 #' @param newdata Data-set to make predictions at.
 #' @param ... Any additional parameters, not currently passed through.
 #'
-#' @import grf
 #'
 #' @return Data-frame with predictions of (E(Y|X,1), E(Y|X,0), E(Y|X,1)-E(Y|X,0))
 #'
@@ -109,7 +115,12 @@ predict.ple_bart = function(object, newdata, ...){
 
   ### Remove Treatment variable if in newdata ##
   X = newdata[,!(colnames(newdata) %in% "A")  ]
-  X.FULL = rbind( data.frame(A=0,X), data.frame(A=1,X) )
+  ## Generate the covariate by treatment interactions ##
+  X_0 = data.frame(0, X, X*0)
+  colnames(X_0) = c( "A", colnames(X), paste(colnames(X), "_A", sep="") )
+  X_1 = data.frame(1, X, X*1)
+  colnames(X_1) = c( "A", colnames(X), paste(colnames(X), "_A", sep="") )
+  X.FULL = rbind( X_0, X_1 )
   preds = predict(object$mods, newdata=X.FULL)
   preds = apply(preds, 2, mean)
   n = dim(X)[1]
