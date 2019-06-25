76f9e046dfa49a3cbb3da4a66dc3218ab58d4efd
Thomas Jemielita <thomasjemielita@gmail.com>
2019-06-25 16:34:01 -0400

    added details

--- R/param_ple.R
+++ R/param_ple.R
@@ -1,7 +1,7 @@
 #' Parameter Estimation: Patient-Level Estimates
 #'
-#'Parameter estimation and inference through patient-level estimates.
-#'Usable for continuous and binary outcomes.
+#' Parameter estimation and inference through patient-level estimates. Usable for continuous
+#' and binary outcomes (possibly survival, needs further evaluation).
 #'
 #' @param Y The outcome variable. Must be numeric or survival (ex; Surv(time,cens) )
 #' @param A Treatment variable. (a=1,...A)
@@ -12,11 +12,11 @@
 #' @param alpha_s Two-sided alpha level at subgroup
 #' @param ... Any additional parameters, not currently passed through.
 #'
-#' @return Data-set with parameter estimates and corresponding
-#' variability metrics, for overall and subgroups.
+#' @return Data-set with parameter estimates and corresponding variability metrics,
+#' for overall and subgroups. Subgrps=0 corresponds to the overall population by default.
 #'  \itemize{
-#'   \item param.dat - Parameter estimates and variability metrics.
-#'   By convention, Subgrps=0 corresponds to overall population.
+#'   \item param.dat - Parameter estimates and variability metrics (est, SE,
+#'   LCL/UCL = lower/upper confidence limits, pval = p-value).
 #'   }
 #' @export
 #' @examples
@@ -30,7 +30,7 @@
 #' train = data.frame(Y, A, X)
 #'
 #' ## Estimate PLEs (ranger) ##
-#' res_ranger = ple_ranger(Y, A, X, Xtest=X)
+#' res_ranger = ple_train(Y, A, X, Xtest=X, ple = "ple_ranger")
 #'
 #' ## Identify Subgroups: MOB (lmtree) ##
 #' res_lmtree = submod_train(Y, A, X, Xtest=X, submod="submod_lmtree")
@@ -46,29 +46,13 @@
 param_ple = function(Y, A, X, mu_hat, Subgrps, alpha_ovrl, alpha_s, ...){
 
   indata = data.frame(Y=Y, A=A, X)
-  ## Overall Estimate ##
-  Y.p = indata$Y
-  A.p = indata$A
-  n = dim(indata)[1]
-  probA = mean(A.p)
-  mu = mu_hat
-  est0 = mean(mu$PLE, na.rm=TRUE)
-  # EIF for variance estimate #
-  eif = ( A.p*Y.p - (A.p-probA)*mu$mu1 )/ probA -
-    ( (1-A.p)*Y.p + (A.p-probA)*mu$mu0 ) / (1-probA) - est0
-  SE0 = sqrt( n^(-2) * sum( eif^2 )  )
-  LCL0 = est0-qt( (1-alpha_ovrl/2), df=n-1 )*SE0
-  UCL0 = est0+qt( (1-alpha_ovrl/2), df=n-1 )*SE0
-  param.dat0 = data.frame(Subgrps=0, N=n, est=est0, SE=SE0, LCL=LCL0, UCL=UCL0)
-  param.dat0$pval = with(param.dat0, 2*pt(-abs(est/SE), df=N-1) )
-
-  ## Subgroup-Specific Estimate ##
+  ## Estimate overall and within subgroups ##
   looper = function(s){
-    Y.s = indata$Y[Subgrps==s]
-    A.s = indata$A[Subgrps==s]
+    Y.s = indata$Y[Subgrps %in% s]
+    A.s = indata$A[Subgrps %in% s]
     n.s = length(Y.s)
     probA = mean(A.s)
-    mu.s = mu_hat[Subgrps==s,]
+    mu.s = mu_hat[Subgrps %in% s,]
     # Average PLEs for point-estimate #
     est = mean(mu.s$PLE, na.rm=TRUE)
     # EIF for variance estimate #
@@ -78,14 +62,19 @@ param_ple = function(Y, A, X, mu_hat, Subgrps, alpha_ovrl, alpha_s, ...){
     LCL = est-qt( (1-alpha_s/2), df=n.s-1 )*SE
     UCL = est+qt( (1-alpha_s/2), df=n.s-1 )*SE
     pval = 2*pt(-abs(est/SE), df=n.s-1)
-    return( c(est, SE, LCL, UCL, pval) )
+    return( c(n.s, est, SE, LCL, UCL, pval) )
   }
+  ## Across subgroups ##
   S_levels = as.numeric( names(table(Subgrps)) )
   S_N = as.numeric( table(Subgrps) )
   param.dat = lapply(S_levels, looper)
   param.dat = do.call(rbind, param.dat)
-  param.dat = data.frame( S = S_levels, N=S_N, param.dat)
+  param.dat = data.frame( S = S_levels, param.dat)
+  # Overall #
+  param.dat0 = looper(s = S_levels)
+  param.dat0 = c(0, param.dat0)
+  param.dat = rbind(param.dat0, param.dat)
   colnames(param.dat) = c("Subgrps", "N", "est", "SE", "LCL", "UCL", "pval")
-  param.dat = rbind( param.dat0, param.dat )
+  # return results #
   return( param.dat )
 }
