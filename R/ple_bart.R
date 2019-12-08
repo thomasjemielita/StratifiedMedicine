bfba26f9a9c22b33a8659bbeaf24e1389fbc2f68
Thomas Jemielita <thomasjemielita@gmail.com>
2019-12-07 22:28:10 -0500

    Binary and survival updates

--- R/ple_bart.R
+++ R/ple_bart.R
@@ -4,11 +4,18 @@
 #' estimates. Used for continuous or binary outcomes. Covariate by treatment interactions
 #' are automatically included in BART model (as in Hahn et al 2017).
 #'
-#' @param Y The outcome variable. Must be numeric or survival (ex; Surv(time,cens) )
+#' @param Y The outcome variable. Must be numeric. 
 #' @param A Treatment variable. (a=1,...A)
 #' @param X Covariate space.
 #' @param Xtest Test set
 #' @param family Outcome type ("gaussian", "binomial"), default is "gaussian"
+#' which uses wbart (BART for continuous outcomes). Probit-based BART ("pbart") is
+#' used for family="binomial"
+#' @param sparse Whether to perform variable selection based on sparse Dirichlet prior
+#' instead of uniform. See "gbart" in the BART R package for more details as well as
+#' Linero 2016. Default is FALSE. 
+#' @param K For survival, coarse times per the quantiles 1/K,..,K/K. For AFT (abart),
+#' K=10. For more accurate survival predictions, set K=100 (default in abart).
 #' @param ... Any additional parameters, not currently passed through.
 #'
 #'
@@ -40,66 +47,144 @@
 #'
 #'
 ### BART ###
-ple_bart = function(Y, A, X, Xtest, family="gaussian", ...){
+ple_bart = function(Y, A, X, Xtest, family="gaussian", sparse=FALSE,
+                    K=10,...){
 
   if (!requireNamespace("BART", quietly = TRUE)) {
     stop("Package BART needed for ple_bart. Please install.")
   }
   if (is.null(A)){
-    mod = BART::wbart(x.train = X, y.train = Y, x.test = Xtest)
-    mu_train = data.frame(PLE = mod$yhat.train.mean)
-    mu_test = data.frame(PLE = mod$yhat.test.mean)
-    pred.fun = function(mod, X){
-      hold = predict(mod, newdata=X)
-      mu_hat = data.frame( PLE = apply(hold, 2, mean)  )
-      return( mu_hat )
+    if (family=="gaussian"){
+      mod <- BART::gbart(x.train = X, y.train = Y, x.test = Xtest,
+                        type = "wbart", sparse = sparse)
+      mu_train <- data.frame(PLE = mod$yhat.train.mean)
+      mu_test <- data.frame(PLE = mod$yhat.test.mean)
+      pred.fun <- function(mod, X){
+        hold <- predict(mod, newdata=X)
+        mu_hat <- data.frame( PLE = apply(hold, 2, mean)  )
+        return( mu_hat )
+      }
+    }
+    if (family=="binomial"){
+      mod <- BART::gbart(x.train = X, y.train = Y, x.test = Xtest,
+                        type = "pbart", sparse = sparse)
+      mu_train <- data.frame(PLE = mod$prob.train.mean)
+      mu_test <- data.frame(PLE = mod$prob.test.mean)
+      pred.fun <- function(mod, X){
+        hold <- predict(mod, newdata=X)
+        mu_hat <- data.frame( PLE = hold$prob.test.mean  )
+        return( mu_hat )
+      }
+    }
+    if (family=="survival"){
+      mod <- BART::abart(x.train = X, times = Y[,1], delta = Y[,2],
+                        x.test = Xtest, sparse = sparse, K=K)
+      mu_train <- data.frame(PLE = mod$yhat.train.mean)
+      mu_test <- data.frame(PLE = mod$yhat.test.mean)
+      pred.fun <- "prediction function not available for survival (abart)"
     }
   }
   if (!is.null(A)){
+    Xmat<- model.matrix(~., data=X)[,-1]
+    X_inter <- Xmat*A
+    colnames(X_inter) <- paste(colnames(Xmat), "_A", sep="")
+    W <- cbind(Xmat, X_inter)
     ## Generate the covariate by treatment interactions ##
-    X_inter = X*A
-    colnames(X_inter) = paste(colnames(X), "_A", sep="")
-    W = cbind(X, A, X_inter)
-    ## Generate counterfactual design matrices  ##
-    Xtrain_0 = data.frame(0, X, X*0)
-    colnames(Xtrain_0) = c( "A", colnames(X), paste(colnames(X), "_A", sep="") )
-    Xtrain_1 = data.frame(1, X, X*1)
-    colnames(Xtrain_1) = c( "A", colnames(X), paste(colnames(X), "_A", sep="") )
-    Xtest_0 = data.frame(0, Xtest, Xtest*0)
-    colnames(Xtest_0) = c( "A", colnames(Xtest), paste(colnames(Xtest), "_A", sep="") )
-    Xtest_1 = data.frame(1, Xtest, Xtest*1)
-    colnames(Xtest_1) = c( "A", colnames(Xtest), paste(colnames(Xtest), "_A", sep="") )
-    # Test: Include both train/test for faster predictions #
-    X.FULL = rbind(Xtrain_0, Xtrain_1, Xtest_0, Xtest_1)
-    ## BART ##
-    mod = BART::wbart(x.train = W, y.train = Y, x.test = X.FULL)
-    n.tr = dim(Xtrain_0)[1]
-    n.ts = dim(Xtest_0)[1]
-    ### PLE Predictions: Train/Test ###
-    mu_train = data.frame(mu1 = mod$yhat.test.mean[(n.tr+1):(2*n.tr)],
-                          mu0 = mod$yhat.test.mean[1:n.tr])
-    mu_train$PLE = mu_train$mu1-mu_train$mu0
-    mu_test = data.frame(mu1 = mod$yhat.test.mean[(2*n.tr+n.ts+1):(2*n.tr+2*n.ts)],
-                         mu0 = mod$yhat.test.mean[(2*n.tr+1):(2*n.tr+n.ts)] )
-    mu_test$PLE = mu_test$mu1 - mu_test$mu0
-    pred.fun = function(mod, X){
+    gen_inters <- function(X){
+      Xmat <- model.matrix(~., data=X)[,-1]
       ## Generate counterfactual design matrices  ##
-      X0 = data.frame(0, X, X*0)
-      colnames(X0) = c( "A", colnames(X), paste(colnames(X), "_A", sep="") )
-      X1 = data.frame(1, X, X*1)
-      colnames(X1) = c( "A", colnames(X), paste(colnames(X), "_A", sep="") )
-      X.FULL = rbind(X0, X1)
-      preds = predict(mod, newdata=X.FULL)
-      preds = apply(preds, 2, mean)
-      n = dim(X)[1]
-      ### PLE Predictions: Train/Test ###
-      mu_hat = data.frame(mu1 = preds[(n+1):(2*n)], mu0 = preds[1:n])
-      mu_hat$PLE = with(mu_hat, mu1-mu0)
-      return( mu_hat )
+      X0 <- data.frame(Xmat, Xmat*0)
+      colnames(X0) <- colnames(W)
+      X1 <- data.frame(Xmat, Xmat*1)
+      colnames(X1) <- colnames(W)
+      # x.test: Include both train/ for faster predictions #
+      X.FULL <- rbind(X0, X1)
+      return( list(W=W, X.FULL=X.FULL) )
     }
+    X.tr <- gen_inters(X)
+    X.ts <- gen_inters(Xtest)
+    X.FULL <- rbind( X.tr$X.FULL, X.ts$X.FULL )
+    ## BART Fit ##
+    if (family=="gaussian"){
+      mod <- BART::gbart(x.train = W, y.train = Y, x.test = X.FULL, 
+                        type = "wbart", sparse=sparse)
+      n.tr <- dim(X)[1]
+      n.ts <- dim(Xtest)[1]
+      ### PLE Predictions: Train/Test ###
+      mu_train <- data.frame(mu1 = mod$yhat.test.mean[(n.tr+1):(2*n.tr)],
+                            mu0 = mod$yhat.test.mean[1:n.tr])
+      mu_train$PLE <- mu_train$mu1-mu_train$mu0
+      mu_test <- data.frame(mu1 = mod$yhat.test.mean[(2*n.tr+n.ts+1):(2*n.tr+2*n.ts)],
+                           mu0 = mod$yhat.test.mean[(2*n.tr+1):(2*n.tr+n.ts)] )
+      mu_test$PLE <- mu_test$mu1 - mu_test$mu0
+      pred.fun <- function(mod, X){
+        ## Generate the covariate by treatment interactions ##
+        Xmat <- model.matrix(~., data=X)[,-1]
+        cnames = c(colnames(Xmat),  paste(colnames(Xmat), "_A", sep="") )
+        ## Generate counterfactual design matrices  ##
+        X0 <- data.frame(Xmat, Xmat*0)
+        colnames(X0) <- cnames
+        X1 <- data.frame(Xmat, Xmat*1)
+        colnames(X1) <- cnames
+        X.FULL <- rbind(X0, X1)
+        preds <- predict(mod, newdata=X.FULL)
+        preds <- apply(preds, 2, mean)
+        n <- dim(X)[1]
+        ### PLE Predictions: Train/Test ###
+        mu_hat <- data.frame(mu1 = preds[(n+1):(2*n)], mu0 = preds[1:n])
+        mu_hat$PLE <- with(mu_hat, mu1-mu0)
+        return( mu_hat )
+      }
+    }
+    if (family=="binomial"){
+      mod <- BART::gbart(x.train = W, y.train = Y, x.test = X.FULL, 
+                        type = "pbart", sparse = sparse)
+      n.tr <- dim(X)[1]
+      n.ts <- dim(Xtest)[1]
+      ### PLE Predictions: Train/Test ###
+      mu_train <- data.frame(mu1 = mod$prob.test.mean[(n.tr+1):(2*n.tr)],
+                            mu0 = mod$prob.test.mean[1:n.tr])
+      mu_train$PLE <- mu_train$mu1-mu_train$mu0
+      mu_test <- data.frame(mu1 = mod$prob.test.mean[(2*n.tr+n.ts+1):(2*n.tr+2*n.ts)],
+                           mu0 = mod$prob.test.mean[(2*n.tr+1):(2*n.tr+n.ts)] )
+      mu_test$PLE = mu_test$mu1 - mu_test$mu0
+      pred.fun = function(mod, X){
+        ## Generate the covariate by treatment interactions ##
+        Xmat <- model.matrix(~., data=X)[,-1]
+        cnames = c(colnames(Xmat),  paste(colnames(Xmat), "_A", sep="") )
+        ## Generate counterfactual design matrices  ##
+        X0 <- data.frame(Xmat, Xmat*0)
+        colnames(X0) <- cnames
+        X1 <- data.frame(Xmat, Xmat*1)
+        colnames(X1) <- cnames
+        X.FULL <- rbind(X0, X1)
+        preds <- predict(mod, newdata=X.FULL)
+        preds <- preds$prob.test.mean
+        n <- dim(X)[1]
+        ### PLE Predictions: Train/Test ###
+        mu_hat <- data.frame(mu1 = preds[(n+1):(2*n)], mu0 = preds[1:n])
+        mu_hat$PLE <- with(mu_hat, mu1-mu0)
+        return( mu_hat )
+      }
+    }
+    if (family=="survival"){
+      mod <- BART::abart(x.train = W, times = Y[,1], delta = Y[,2],
+                        x.test = X.FULL, sparse = sparse, K=K)
+      n.tr <- dim(X)[1]
+      n.ts <- dim(Xtest)[1]
+      ### PLE Predictions: Train/Test ###
+      mu_train <- data.frame(mu1 = mod$yhat.test.mean[(n.tr+1):(2*n.tr)],
+                            mu0 = mod$yhat.test.mean[1:n.tr])
+      mu_train$PLE <- mu_train$mu1-mu_train$mu0
+      mu_test <- data.frame(mu1 = mod$yhat.test.mean[(2*n.tr+n.ts+1):(2*n.tr+2*n.ts)],
+                           mu0 = mod$yhat.test.mean[(2*n.tr+1):(2*n.tr+n.ts)] )
+      mu_test$PLE <- mu_test$mu1 - mu_test$mu0
+      pred.fun <- "prediction function not available for survival (abart)"
+    }
+    
   }
-  res = list(mod = mod, pred.fun=pred.fun, mu_train = mu_train, mu_test=mu_test, A=A)
-  class(res) = "ple_bart"
+  res <- list(mod = mod, pred.fun=pred.fun, mu_train = mu_train, mu_test=mu_test)
+  class(res) <- "ple_bart"
   ## Return Results ##
   return( res )
 }
