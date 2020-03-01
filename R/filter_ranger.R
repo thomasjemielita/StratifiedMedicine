50361cc09afd792e497cbaf3e165214eea948ad5
Thomas Jemielita <thomasjemielita@gmail.com>
2020-03-01 01:44:31 -0500

    Update doc

--- R/filter_ranger.R
+++ R/filter_ranger.R
@@ -2,8 +2,8 @@
 #'
 #' Filtering through Random Forest Variable Importance with p-values. P-values are obtained
 #' through subsampling based T-statistics, as described in Ishwaran and Lu 2017. Default is
-#' to remove variables with p-values >= 0.10. Used for continuous, binary, or survival
-#' outcomes.
+#' to remove variables if one-sided (VI>0) p-values >= 0.10. Used for continuous, binary, 
+#' or survival outcomes.
 #'
 #' @param Y The outcome variable. Must be numeric or survival (ex; Surv(time,cens) )
 #' @param A Treatment variable. (a=1,...A)
@@ -23,6 +23,9 @@
 #' }
 #'
 #' @export
+#' @references Wright, M. N. & Ziegler, A. (2017). ranger: A fast implementation of 
+#' random forests for high dimensional data in C++ and R. J Stat Softw 77:1-17. 
+#' \url{https://doi.org/10.18637/jss.v077.i01}.
 #' @examples
 #' library(StratifiedMedicine)
 #'
@@ -41,14 +44,14 @@
 #'
 ##### RF Variable Importance ######
 filter_ranger = function(Y, A, X, b=0.66, K=200, DF2=FALSE, FDR=FALSE, pval.thres=0.10,
-                     family="gaussian", ...){
+                     family="gaussian", ...) {
 
-  if (DF2==TRUE){ #Generate the interaction covariates #
+  if (DF2==TRUE) { #Generate the interaction covariates #
     X_inter = X*A
     colnames(X_inter) = paste(colnames(X), "_A", sep="")
     W = cbind(X, A, X_inter)
   }
-  if (DF2==FALSE){
+  if (DF2==FALSE) {
     W = cbind(X)
   }
 
@@ -58,7 +61,7 @@ filter_ranger = function(Y, A, X, b=0.66, K=200, DF2=FALSE, FDR=FALSE, pval.thre
 
   ### Subsample function ###
   b1 = ( dim(X)[1] )^(b)
-  sub_VI = function(s){
+  sub_VI = function(s) {
     ## Subsample data ##
     hold = data.frame(Y, W)
     hold = hold[sample(nrow(hold), size=b1),]
@@ -70,57 +73,79 @@ filter_ranger = function(Y, A, X, b=0.66, K=200, DF2=FALSE, FDR=FALSE, pval.thre
   res = do.call(rbind, res)
   VI.var = NULL
   for (j in 1:dim(res)[2]){
-    VI.var = c(VI.var, b1/((length(Y)-b1)*K) * sum( (res[,j] - VI0[j])^2, na.rm = TRUE ) )
+    VI.var = c(VI.var, b1/((length(Y)-b1)*K) * sum((res[,j] - VI0[j])^2, na.rm = TRUE ))
   }
   #### Initial Output of VI and SE(VI) for each variable ###
   out = data.frame(Variables = colnames(W), est = VI0, SE = sqrt(VI.var) )
-  if (DF2==FALSE){
+  if (DF2==FALSE) {
     out.F = out
     ### T-Statistics and one-sided p-values ###
-    out.F$Tstat = with(out.F, est / SE )
-    out.F$pval = with(out.F, 1-pnorm(Tstat) )
+    out.F$Tstat = with(out.F, est / SE)
+    out.F$pval = with(out.F, 1-pnorm(Tstat))
     out.F$pval.fdr = p.adjust(out.F$pval, "fdr")
   }
-  if (DF2==TRUE){
+  if (DF2==TRUE) {
     out.F = data.frame(Variables=colnames(X), est.2DF=NA, SE.2DF=NA)
-    for (var in colnames(X)){
-      #print(var)
-      #var = "X187"
+    for (var in colnames(X)) {
       inter.name = paste(var, "_A", sep="")
-      ## Extract VI estimates and SEs ##
+      # Extract VI estimates and SEs #
       est0 = out$est[out$Variables==var]; SE0 = out$SE[out$Variable==var]
       estI = out$est[out$Variables==inter.name]; SEI = out$SE[out$Variable==inter.name]
-      ## Calculate empirical covariance between main/interaction VIs ##
+      # Calculate empirical covariance between main/interaction VIs #
       indices = match(c(var, inter.name), colnames(W))
       cov.est = b1/((length(Y)-b1)*K) *
-        sum( (res[,indices[1]]-est0)*(res[,indices[2]]-estI), na.rm=TRUE)
-      ## Calculcate "2 DF" VI Estimate and SE ##
+        sum((res[,indices[1]]-est0)*(res[,indices[2]]-estI), na.rm=TRUE)
+      # Calculcate "2 DF" VI Estimate and SE #
       est.F = estI+est0
       SE.F = sqrt( SE0^2 + SEI^2 + 2*cov.est )
-      ## Store ##
+      # Store #
       out.F$est.2DF[out.F$Variables==var] =  est.F
       out.F$SE.2DF[out.F$Variables==var] =  SE.F
     }
-    ### T-Statistics and One-sided Pval ###
-    out.F$Tstat = with(out.F, est.2DF / SE.2DF)
-    out.F$pval = with(out.F, 1-pnorm(Tstat) )
+    # T-Statistics and One-sided Pval #
+    out.F$Tstat = with(out.F, est.2DF/SE.2DF)
+    out.F$pval = with(out.F, 1-pnorm(Tstat))
     out.F$pval.fdr = p.adjust(out.F$pval, "fdr")
   }
 
-  ## FDR Adjustment? ##
-  if (FDR){
-    out.F$filter_vars = with(out.F, ifelse(pval.fdr<pval.thres, 1, 0) )
+  # FDR Adjustment? #
+  if (FDR) {
+    out.F$filter_vars = with(out.F, ifelse(pval.fdr<pval.thres, 1, 0))
   }
-  if (!FDR){
-    out.F$filter_vars = with(out.F, ifelse(pval<pval.thres, 1, 0) )
+  if (!FDR) {
+    out.F$filter_vars = with(out.F, ifelse(pval<pval.thres, 1, 0))
   }
-  # ggplot(out.F[out.F$pval<0.05,], aes(x=reorder(Variables,-pval), y=est)) +
-  #   geom_errorbar(aes(ymin=est-1.96*SE, ymax=est+1.96*SE), width=.1)+geom_point()+
-  #   ylab("Variable Importance")+
-  #   xlab("Variables")+
-  #   ggtitle("Variable Importance with 95% CI")+coord_flip()
-  ## Return VI information and variables that we filter ##
-  mod = out.F
-  filter.vars = as.character( mod$Variables[mod$filter_vars==1] )
-  return( list(mod=mod, filter.vars=filter.vars) )
+  out.F$LCL <- with(out.F, est-qnorm(1-pval.thres)*SE)
+  out.F$UCL <- with(out.F, est+qnorm(1-pval.thres)*SE)
+  filter.vars = with(out.F, as.character(Variables[filter_vars==1]))
+  out.F$rank <- with(out.F, rank(pval))
+  out.F$pval.thres <- pval.thres
+  VI.dat <- out.F
+  VI.dat$family <- family
+  mod = list(VI.dat=VI.dat)
+  # Return VI information and variables that we filter #
+  return(list(mod=mod, filter.vars=filter.vars) )
+}
+# VIMP Plot #
+plot_vimp_ranger <- function(mod, top_n=NULL) {
+  VI.dat <- mod$VI.dat
+  family <- unique(VI.dat$family)
+  pval.thres <- unique(VI.dat$pval.thres)
+  plot.title <- paste("Random Forest (", family,
+                      ") Importance Plot", sep="")
+  y.label <- paste("Variable Importance ", 
+                   "(", (1-pval.thres)*100, "% CI)",sep="")
+  if (!is.null(top_n)) {
+    VI.dat <- VI.dat[VI.dat$rank<=top_n,]
+    plot.title <- paste(plot.title, " [Top ", top_n, "]", sep="")
+  }
+  vimp.plt <- ggplot2::ggplot(VI.dat, aes(x=reorder(.data$Variables,-.data$pval), 
+                                 y=.data$est, ymin=.data$LCL, ymax=.data$UCL)) +
+    ggplot2::geom_errorbar(width=.1)+geom_point()+
+    ggplot2::geom_hline(yintercept=0, color="red")+
+    ggplot2::ylab(y.label)+
+    ggplot2::xlab("Variable")+
+    ggplot2::ggtitle(plot.title)+
+    ggplot2::coord_flip()
+  return(vimp.plt)
 }
